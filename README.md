6e35fce22672ad934095d88ff7814fe0f4e9ab01
Aung Ko Man <aungkoman@gmail.com>
2025-06-10 13:34:15 +0630

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Note Taking App using Jetpack Compose
+
+Jetpack Compose ကို အစက ပြန်နွှေးရချေဉီးမယ် တစ်မုံ့။
+
---
+++ app/src/main/java/mm/com/sofware100/notetakingapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package mm.com.sofware100.notetakingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mm.com.sofware100.notetakingapp.ui.theme.NoteTakingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NoteTakingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = " World",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteTakingTheme {
+        Greeting("Android")
+    }
+}
